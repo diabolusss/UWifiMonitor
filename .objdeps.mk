f20f44654addfc401487335112aa9ff5e81b5645
Vitaly Hodiko <hodiko.v@inbox.lv>
2017-02-08 22:11:38 +0200

    Initial project commit

---
+++ .objdeps.mk
@@ -0,0 +1,64 @@
+io_log.o: io_log.c io_log.h /usr/local/include/uwifi/wlan_parser.h \
+ /usr/local/include/uwifi/wlan80211.h /usr/local/include/uwifi/channel.h \
+ /usr/local/include/uwifi/util.h utils_net.h \
+ /usr/local/include/uwifi/util.h main.h \
+ /usr/local/include/uwifi/channel.h /usr/local/include/uwifi/conf.h \
+ /usr/local/include/uwifi/ccan/list/list.h \
+ /usr/local/include/uwifi/ccan/str/str.h \
+ /usr/local/include/uwifi/config.h \
+ /usr/local/include/uwifi/ccan/str/str_debug.h \
+ /usr/local/include/uwifi/ccan/container_of/container_of.h \
+ /usr/local/include/uwifi/ccan/check_type/check_type.h \
+ /usr/local/include/uwifi/platform.h
+utils_custom.o: utils_custom.c io_log.h \
+ /usr/local/include/uwifi/wlan_parser.h \
+ /usr/local/include/uwifi/wlan80211.h /usr/local/include/uwifi/channel.h \
+ /usr/local/include/uwifi/util.h utils_custom.h \
+ /usr/local/include/uwifi/util.h
+utils_time.o: utils_time.c utils_time.h
+utils_net.o: utils_net.c utils_net.h
+network.o: network.c network.h main.h /usr/local/include/uwifi/channel.h \
+ /usr/local/include/uwifi/conf.h \
+ /usr/local/include/uwifi/ccan/list/list.h \
+ /usr/local/include/uwifi/ccan/str/str.h \
+ /usr/local/include/uwifi/config.h \
+ /usr/local/include/uwifi/ccan/str/str_debug.h \
+ /usr/local/include/uwifi/ccan/container_of/container_of.h \
+ /usr/local/include/uwifi/ccan/check_type/check_type.h \
+ /usr/local/include/uwifi/wlan80211.h /usr/local/include/uwifi/channel.h \
+ /usr/local/include/uwifi/platform.h \
+ /usr/local/include/uwifi/wlan_parser.h /usr/local/include/uwifi/util.h \
+ io_log.h
+conf_options.o: conf_options.c main.h /usr/local/include/uwifi/channel.h \
+ /usr/local/include/uwifi/conf.h \
+ /usr/local/include/uwifi/ccan/list/list.h \
+ /usr/local/include/uwifi/ccan/str/str.h \
+ /usr/local/include/uwifi/config.h \
+ /usr/local/include/uwifi/ccan/str/str_debug.h \
+ /usr/local/include/uwifi/ccan/container_of/container_of.h \
+ /usr/local/include/uwifi/ccan/check_type/check_type.h \
+ /usr/local/include/uwifi/wlan80211.h /usr/local/include/uwifi/channel.h \
+ /usr/local/include/uwifi/platform.h io_log.h \
+ /usr/local/include/uwifi/wlan_parser.h /usr/local/include/uwifi/util.h \
+ /usr/local/include/uwifi/util.h /usr/local/include/uwifi/wlan_util.h \
+ conf_options.h
+main.o: main.c /usr/local/include/uwifi/radiotap.h \
+ /usr/local/include/uwifi/radiotap_iter.h \
+ /usr/local/include/uwifi/radiotap.h /usr/local/include/uwifi/ifctrl.h \
+ /usr/local/include/uwifi/channel.h /usr/local/include/uwifi/conf.h \
+ /usr/local/include/uwifi/ccan/list/list.h \
+ /usr/local/include/uwifi/ccan/str/str.h \
+ /usr/local/include/uwifi/config.h \
+ /usr/local/include/uwifi/ccan/str/str_debug.h \
+ /usr/local/include/uwifi/ccan/container_of/container_of.h \
+ /usr/local/include/uwifi/ccan/check_type/check_type.h \
+ /usr/local/include/uwifi/wlan80211.h /usr/local/include/uwifi/platform.h \
+ /usr/local/include/uwifi/util.h /usr/local/include/uwifi/netdev.h \
+ /usr/local/include/uwifi/capture.h /usr/local/include/uwifi/channel.h \
+ main.h io_log.h /usr/local/include/uwifi/wlan_parser.h \
+ /usr/local/include/uwifi/util.h utils_custom.h \
+ /usr/local/include/uwifi/raw_parser.h \
+ /usr/local/include/uwifi/wlan_parser.h \
+ /usr/local/include/uwifi/wlan_util.h \
+ /usr/local/include/uwifi/ieee80211_util.h network.h utils_time.h \
+ conf_options.h
